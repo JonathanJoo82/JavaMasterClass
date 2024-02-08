19e4cdbe10f086145f75ca6f024f437fb676b87d
JonathanJoo82 <jonathanjoo@gmail.com>
2024-02-07 18:27:38 -0600

    first set up

---
+++ Java Masterclass_Start Coding/src/Section4/Main.java
@@ -0,0 +1,13 @@
+package Section4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    //usages of operators and operends.
+    //defining statements and expressions
+    //primitive type variables
+    //casting in java
+
+}
