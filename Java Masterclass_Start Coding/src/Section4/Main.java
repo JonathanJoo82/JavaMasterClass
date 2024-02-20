0621cd39353788bf912baaf4ed0c0c83627c7c1c
JonathanJoo82 <jonathanjoo@gmail.com>
2024-02-19 18:30:01 -0600

    test

--- Java Masterclass_Start Coding/src/Section4/Main.java
+++ Java Masterclass_Start Coding/src/Section4/Main.java
@@ -3,6 +3,23 @@ package Section4;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //System.out.println("primitive types");
+        //char boolean float double long int float short
+        //non primitive types: string
+
+
+        /*
+        basic operators and conditional checks can be found easily through google and manya operators are the same depending on the operands
+         */
+
+
+        String test1 = "the boolean is true";
+        String test2 = "the boolean is false";
+
+        boolean test = false;
+        System.out.println(!test ? test1 : test2);
+
     }
 
     //usages of operators and operends.
